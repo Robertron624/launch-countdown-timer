5e603e6f6c68bb4770e6cf471fe7eabf7e93684b
Roberto Ramirez <robert.624@hotmail.com>
2023-06-16 12:23:27 -0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+
+</script>
+
+<main>
+  <h3>
+    Hello World!
+  </h3>
+
+</main>
+
+<style lang="scss">
+
+  @import './variables.scss';
+
+  $white : #fff;
+
+  h3 {
+    color: $soft-red;
+  }
+
+</style>
